1ec16299b7c7702db579b40ce58750ac06cd5b99
mcjazzyfunky <mcjazzyfunky@users.noreply.github.com>
2018-04-27 23:28:06 +0200

    Revert "Some updates"
    This reverts commit 8445c33eb82f8123a7d84e1edbc04bdb7a3e4401.

--- src/main/api/isElement.ts
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-
-const specHintSymbol = Symbol.for('js-spec:hint');
-
-export default function isElement(it) {
-    return React.isValidElement(it);
-}
-
-isElement[specHintSymbol] = 'Must be a React element';
